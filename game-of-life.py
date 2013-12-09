50ee686d0ec9522746775e4796b73db19f0a9190
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2013-12-09 20:22:56 +0000

    Created a new class so that multiple "games of life" can be instantiated.

--- game-of-life.py
+++ game-of-life.py
@@ -21,73 +21,84 @@ import numpy
 import pylab
 import random
 
-# Set up two grids to hold the old and new configurations.
-# This assumes an N*N grid of points.
-# Each point is either alive or dead, represented by integer values of 1 and 0, respectively.
-N = 100
-old_grid = numpy.zeros(N*N, dtype='i').reshape(N,N)
-new_grid = numpy.zeros(N*N, dtype='i').reshape(N,N)
+class GameOfLife:
 
-def seed(grid):
-   """ Set up a random initial configuration for the grid. """
-   for i in range(0, N):
-      for j in range(0, N):
-         if(random.randint(0, 100) < 15):
-            grid[i][j] = 1
-         else:
-            grid[i][j] = 0
-         
-def live_neighbours(grid, i, j):
-   """ Count the number of live neighbours around point (i, j). """
-   s = 0 # The total number of live neighbours.
-   # Loop over all the neighbours.
-   for x in [i-1, i, i+1]:
-      for y in [j-1, j, j+1]:
-         if(x == i and y == j):
-            continue # Skip the current point itself - we only want to count the neighbours!
-         if(x != N and y != N):
-            s += grid[x][y]
-         # The remaining branches handle the case where the neighbour is off the end of the grid.
-         # In this case, we loop back round such that the grid becomes a "toroidal array".
-         elif(x == N and y != N):
-            s += grid[0][y]
-         elif(x != N and y == N):
-            s += grid[x][0]
-         else:
-            s += grid[0][0]
-   return s
+   def __init__(self, N=100, T=200):
+      """ Set up Conway's Game of Life. """
+      # Here we create two grids to hold the old and new configurations.
+      # This assumes an N*N grid of points.
+      # Each point is either alive or dead, represented by integer values of 1 and 0, respectively.
+      self.N = N
+      self.old_grid = numpy.zeros(N*N, dtype='i').reshape(N,N)
+      self.new_grid = numpy.zeros(N*N, dtype='i').reshape(N,N)
+      self.T = T # The maximum number of generations
 
-# Seed an initial configuration for the grid
-seed(old_grid)
-pylab.pcolormesh(old_grid)
-pylab.colorbar()
-pylab.savefig("generation0.png")
+      # Set up a random initial configuration for the grid.
+      for i in range(0, self.N):
+         for j in range(0, self.N):
+            if(random.randint(0, 100) < 15):
+               self.old_grid[i][j] = 1
+            else:
+               self.old_grid[i][j] = 0
+      
+   def live_neighbours(self, i, j):
+      """ Count the number of live neighbours around point (i, j). """
+      s = 0 # The total number of live neighbours.
+      # Loop over all the neighbours.
+      for x in [i-1, i, i+1]:
+         for y in [j-1, j, j+1]:
+            if(x == i and y == j):
+               continue # Skip the current point itself - we only want to count the neighbours!
+            if(x != self.N and y != self.N):
+               s += self.old_grid[x][y]
+            # The remaining branches handle the case where the neighbour is off the end of the grid.
+            # In this case, we loop back round such that the grid becomes a "toroidal array".
+            elif(x == self.N and y != self.N):
+               s += self.old_grid[0][y]
+            elif(x != self.N and y == self.N):
+               s += self.old_grid[x][0]
+            else:
+               s += self.old_grid[0][0]
+      return s
 
-t = 1; T = 200 # Time-stepping parameters.
-write_frequency = 5 # How frequently we want to output a grid configuration.
-while t <= T: # Evolve!
-   print "At time level %d" % t
-   # Loop over each cell of the grid and apply Conway's rules.
-   for i in range(N):
-      for j in range(N):
-         live = live_neighbours(old_grid, i, j)
-         if(old_grid[i][j] == 1 and live < 2):
-            new_grid[i][j] = 0 # Dead from starvation.
-         elif(old_grid[i][j] == 1 and (live == 2 or live == 3)):
-            new_grid[i][j] = 1 # Continue living.
-         elif(old_grid[i][j] == 1 and live > 3):
-            new_grid[i][j] = 0 # Dead from overcrowding.
-         elif(old_grid[i][j] == 0 and live == 3):
-            new_grid[i][j] = 1 # Alive from reproduction.
+   def play(self):
+      """ Play Conway's Game of Life. """
 
-   # Output the new configuration.
-   if(t % write_frequency == 0):
-      pylab.pcolormesh(new_grid)
-      pylab.savefig("generation%d.png" % t)
+      # Write the initial configuration to file.
+      pylab.pcolormesh(self.old_grid)
+      pylab.colorbar()
+      pylab.savefig("generation0.png")
 
-   # The new configuration becomes the old configuration for the next generation.
-   old_grid = new_grid.copy()
+      t = 1 # Current time level
+      write_frequency = 5 # How frequently we want to output a grid configuration.
+      while t <= self.T: # Evolve!
+         print "At time level %d" % t
 
-   # Move on to the next time level
-   t += 1
-       
+         # Loop over each cell of the grid and apply Conway's rules.
+         for i in range(self.N):
+            for j in range(self.N):
+               live = self.live_neighbours(i, j)
+               if(self.old_grid[i][j] == 1 and live < 2):
+                  self.new_grid[i][j] = 0 # Dead from starvation.
+               elif(self.old_grid[i][j] == 1 and (live == 2 or live == 3)):
+                  self.new_grid[i][j] = 1 # Continue living.
+               elif(self.old_grid[i][j] == 1 and live > 3):
+                  self.new_grid[i][j] = 0 # Dead from overcrowding.
+               elif(self.old_grid[i][j] == 0 and live == 3):
+                  self.new_grid[i][j] = 1 # Alive from reproduction.
+
+         # Output the new configuration.
+         if(t % write_frequency == 0):
+            pylab.pcolormesh(self.new_grid)
+            pylab.savefig("generation%d.png" % t)
+
+         # The new configuration becomes the old configuration for the next generation.
+         self.old_grid = self.new_grid.copy()
+
+         # Move on to the next time level
+         t += 1
+
+if(__name__ == "__main__"):
+   game = GameOfLife(N = 100, T = 200)
+   game.play()
+
